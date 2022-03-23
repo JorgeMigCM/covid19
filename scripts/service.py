c997990073ae609c1aacb3ad9029d3e89d2c572d
JorgeMig <40581398+JorgeMigCM@users.noreply.github.com>
2022-03-23 13:40:10 -0400

    Update service.py

--- scripts/service.py
+++ scripts/service.py
@@ -1,4 +1,3 @@
-
 #Import Flask
 from flask import Flask, request, jsonify, redirect
 from flask_cors import CORS
@@ -21,6 +20,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 port = int(os.getenv('PORT', 5000))
 print ("Port recognized: ", port)
 
+#Initialize the application service
 app = Flask(__name__)
 CORS(app)
 global loaded_model, graph
@@ -30,23 +30,27 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+#Define a route
 @app.route('/')
 def main_page():
-	return 'Servicios Activados'
+	return '¡Servicio REST activo!'
 
 @app.route('/model/covid19/', methods=['GET','POST'])
 def default():
     data = {"success": False}
     if request.method == "POST":
+        # check if the post request has the file part
         if 'file' not in request.files:
             print('No file part')
         file = request.files['file']
+        # if user does not select file, browser also submit a empty part without filename
         if file.filename == '':
             print('No selected file')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+            #loading image
             filename = UPLOAD_FOLDER + '/' + filename
             print("\nfilename:",filename)
 
@@ -58,9 +62,11 @@ def default():
 
             with graph.as_default():
             	result = loaded_model.predict(test_image)[0][0]
-
+            	# print(result)
+            	
+		# Resultados
             	prediction = 1 if (result >= 0.5) else 0
-            	CLASSES = ['Pulmones Normales', 'Covid19 positivo']
+            	CLASSES = ['Normal', 'Covid19+']
 
             	ClassPred = CLASSES[prediction]
             	ClassProb = result
@@ -70,11 +76,13 @@ def default():
 
             	#Results as Json
             	data["predictions"] = []
-            	r = {"Diagnostico": ClassPred, "Porcentaje de acierto": float(ClassProb)}
+            	r = {"label": ClassPred, "score": float(ClassProb)}
             	data["predictions"].append(r)
 
+            	#Success
             	data["success"] = True
 
     return jsonify(data)
 
+# Run de application
 app.run(host='0.0.0.0',port=port, threaded=False)
